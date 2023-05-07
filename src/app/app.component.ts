7b6803fd595854ff44099ce2932050de430ab33a
Ali Can Yücel <yucelalican30@gmail.com>
2023-05-07 13:06:15 +0300

    ..

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,20 +7,22 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Ali Can Yücel Angular Xox Game';
   mark: string = "";
+  moves:any=[];
   message: string = "";
   games: string[] = ["", "", "", "", "", "", "", "", ""];
   constructor() {
     this.newGame();
   }
   newGame() {
-    this.mark = "";
-    this.message = "";
+    this.mark = "X";
+    this.message = "Sıradaki:X";
     this.games = ["", "", "", "", "", "", "", "", ""];
 
   }
   setMark(index: number) {
     // fonksiyon içindeki parametreyi kullanmak için [parametre] bu şekilde
     this.games[index] = this.mark;
+    this.moves.push([...this.games]);
     if (this.mark == "X")
       this.mark = "O"
     else
