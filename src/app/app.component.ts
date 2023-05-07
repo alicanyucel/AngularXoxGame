9bcc0c2362000b5a77d27a07c1971b9b1533388a
Ali Can Yücel <yucelalican30@gmail.com>
2023-05-07 15:17:07 +0300

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -35,52 +35,52 @@ export class AppComponent {
   isGameOver() {
     if (this.games[0].mark != "" && this.games[0].mark == this.games[1].mark && this.games[1].mark == this.games[2].mark) {
       this.gameOver = true;
-     this.games[0]=true
-     this.games[1]=true
-     this.games[2]=true
+     this.games[0].winner=true
+     this.games[1].winner=true
+     this.games[2].winner=true
     }
     if (this.games[3].mark != "" && this.games[3].mark == this.games[4].mark && this.games[4].mark == this.games[5].mark) {
       this.gameOver = true;
-      this.games[3]=true
-      this.games[4]=true
-      this.games[5]=true
+      this.games[3].winner=true
+      this.games[4].winner=true
+      this.games[5].winner=true
 
     }
     if (this.games[6].mark != "" && this.games[6].mark == this.games[7].mark && this.games[7].mark == this.games[8].mark) {
       this.gameOver = true;
-      this.games[6]=true
-      this.games[7]=true
-      this.games[8]=true
+      this.games[6].winner=true
+      this.games[7].winner=true
+      this.games[8].winner=true
     }
     if (this.games[0].mark != "" && this.games[0].mark == this.games[3].mark && this.games[3].mark == this.games[6].mark) {
       this.gameOver = true;
-      this.games[0]=true
-      this.games[3]=true
-      this.games[6]=true
+      this.games[0].winner=true
+      this.games[3].winner=true
+      this.games[6].winner=true
     }
     if (this.games[1].mark != "" && this.games[1].mark == this.games[4].mark && this.games[4].mark == this.games[7].mark) {
       this.gameOver = true;
-      this.games[1]=true
-      this.games[4]=true
-      this.games[7]=true
+      this.games[1].winner=true
+      this.games[4].winner=true
+      this.games[7].winner=true
     }
     if (this.games[2].mark != "" && this.games[2].mark == this.games[5].mark && this.games[5].mark == this.games[8].mark) {
       this.gameOver = true;
-      this.games[2]=true
-      this.games[5]=true
-      this.games[8]=true
+      this.games[2].winner=true
+      this.games[5].winner=true
+      this.games[8].winner=true
     }
     if (this.games[0].mark != "" && this.games[0].mark == this.games[4].mark && this.games[4].mark == this.games[8].mark) {
       this.gameOver = true;
-      this.games[0]=true
-      this.games[4]=true
-      this.games[8]=true
+      this.games[0].winner=true
+      this.games[4].winner=true
+      this.games[8].winner=true
     }
     if (this.games[2].mark != "" && this.games[2].mark == this.games[4].mark && this.games[4].mark == this.games[6].mark) {
       this.gameOver = true;
-      this.games[2]=true
-      this.games[4]=true
-      this.games[6]=true
+      this.games[2].winner=true
+      this.games[4].winner=true
+      this.games[6].winner=true
     }
   }
 changeWinnerButtonClass(winner:boolean)
