016dc0c92520571706bf27181b73840ae00e4131
Ali Can Yücel <yucelalican30@gmail.com>
2023-05-07 18:38:27 +0300

    süper süper süper

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,7 @@ export class AppComponent {
   mark: string = "";
   moves: any = [];
   message: string = "";
+  isMoveFinish: boolean = false;
   gameOver: boolean = false;
   games: any[] = [
   ];
@@ -30,68 +31,77 @@ export class AppComponent {
       { mark: "", winner: false },
       { mark: "", winner: false },];
     this.gameOver = false;
-    this.moves=[];
+    this.moves = [];
+    this.isMoveFinish=false;
   }
   isGameOver() {
     if (this.games[0].mark != "" && this.games[0].mark == this.games[1].mark && this.games[1].mark == this.games[2].mark) {
       this.gameOver = true;
-     this.games[0].winner=true
-     this.games[1].winner=true
-     this.games[2].winner=true
+      this.games[0].winner = true
+      this.games[1].winner = true
+      this.games[2].winner = true
     }
     if (this.games[3].mark != "" && this.games[3].mark == this.games[4].mark && this.games[4].mark == this.games[5].mark) {
       this.gameOver = true;
-      this.games[3].winner=true
-      this.games[4].winner=true
-      this.games[5].winner=true
+      this.games[3].winner = true
+      this.games[4].winner = true
+      this.games[5].winner = true
 
     }
     if (this.games[6].mark != "" && this.games[6].mark == this.games[7].mark && this.games[7].mark == this.games[8].mark) {
       this.gameOver = true;
-      this.games[6].winner=true
-      this.games[7].winner=true
-      this.games[8].winner=true
+      this.games[6].winner = true
+      this.games[7].winner = true
+      this.games[8].winner = true
     }
     if (this.games[0].mark != "" && this.games[0].mark == this.games[3].mark && this.games[3].mark == this.games[6].mark) {
       this.gameOver = true;
-      this.games[0].winner=true
-      this.games[3].winner=true
-      this.games[6].winner=true
+      this.games[0].winner = true
+      this.games[3].winner = true
+      this.games[6].winner = true
     }
     if (this.games[1].mark != "" && this.games[1].mark == this.games[4].mark && this.games[4].mark == this.games[7].mark) {
       this.gameOver = true;
-      this.games[1].winner=true
-      this.games[4].winner=true
-      this.games[7].winner=true
+      this.games[1].winner = true
+      this.games[4].winner = true
+      this.games[7].winner = true
     }
     if (this.games[2].mark != "" && this.games[2].mark == this.games[5].mark && this.games[5].mark == this.games[8].mark) {
       this.gameOver = true;
-      this.games[2].winner=true
-      this.games[5].winner=true
-      this.games[8].winner=true
+      this.games[2].winner = true
+      this.games[5].winner = true
+      this.games[8].winner = true
     }
     if (this.games[0].mark != "" && this.games[0].mark == this.games[4].mark && this.games[4].mark == this.games[8].mark) {
       this.gameOver = true;
-      this.games[0].winner=true
-      this.games[4].winner=true
-      this.games[8].winner=true
+      this.games[0].winner = true
+      this.games[4].winner = true
+      this.games[8].winner = true
     }
     if (this.games[2].mark != "" && this.games[2].mark == this.games[4].mark && this.games[4].mark == this.games[6].mark) {
       this.gameOver = true;
-      this.games[2].winner=true
-      this.games[4].winner=true
-      this.games[6].winner=true
+      this.games[2].winner = true
+      this.games[4].winner = true
+      this.games[6].winner = true
     }
   }
-changeWinnerButtonClass(winner:boolean)
-{
-if(winner)
-  return "btn-success";
-  else
-  return "btn-warning"
-}
+  changeWinnerButtonClass(winner: boolean) {
+    if (winner)
+      return "btn-success";
+    else
+      return "btn-warning"
+  }
   returnSelectMove(index: number) {
     this.games = this.moves[index];
+  }
+  checkGameMoveIsOver() {
+    this.isMoveFinish=true;
+    for (let i = 0; i < this.games.length; i++) {
+      if (this.games[i].mark == "") {
+        this.isMoveFinish=false;
+        break;
+      }
+    }
   }
   setMark(index: number) {
     // fonksiyon içindeki parametreyi kullanmak için [parametre] bu şekilde
@@ -99,8 +109,13 @@ if(winner)
       this.games[index].mark = this.mark;
       this.moves.push([...this.games]);
       this.isGameOver();
+      this.checkGameMoveIsOver();
       if (this.gameOver) {
         this.message = "Oyun Bitti.Kazanan:" + this.mark
+      }
+      else if(this.isMoveFinish)
+      {
+        this.message = "oyun berabere bitti."
       }
       else {
         if (this.mark == "X")
